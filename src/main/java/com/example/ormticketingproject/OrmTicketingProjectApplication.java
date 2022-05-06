b4205ebd8cb3b4b399fd10b1bfa4fb3120429aec
n7owhad <n7owhad@hotmail.com>
2022-05-06 03:03:04 -0400

    Initial commit

---
+++ src/main/java/com/example/ormticketingproject/OrmTicketingProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.ormticketingproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrmTicketingProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrmTicketingProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/ormticketingproject/OrmTicketingProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ormticketingproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrmTicketingProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
